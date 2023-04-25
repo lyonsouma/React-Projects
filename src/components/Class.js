fc6ee8de3b9a438c9cbf86a55b3040d8d5a87ddf
Lyons Ouma <oumalyons26@gmail.com>
2023-04-25 17:36:17 +0300

    Introduction to react

---
+++ src/components/Class.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class Lyons extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello </h1>
+            </div>
+        )
+    }
+}
+export default Lyons;
---
+++ src/components/Message.js
@@ -0,0 +1,10 @@
+// import React from "react";
+
+function Message(){
+return(
+        <div>
+            <h1>Welcome to my react app</h1>
+        </div>
+);
+}
+export default Message;
