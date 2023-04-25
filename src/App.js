d9c05e6a09c67629fa2bd0fd7b237c0879a76d69
Lyons Ouma <oumalyons26@gmail.com>
2023-04-25 19:31:25 +0300

    setState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
-import Message from './components/Message';
-import Lyons from './components/Class';
-
+// import Message from './components/Message';
+// import Lyons from './components/Class';
+import Subscribe from './components/state';
 
 function App() {
   return (
     <div className="App">
-         <h1>Hi Guys</h1>
+       <Subscribe/>
+         {/* <h1>Hi Guys</h1>
          <Message name = "Kevin"></Message>
          <Message name = "Mercy"></Message>
          <Message name = "Lyons"></Message>
         <Lyons name="John" />
-        <button>Click here</button>
+        <button>Click here</button> */}
     </div>
     
   );
