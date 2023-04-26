5fc3be16be6698402c30e00ef4e0874452fa9c6c
Lyons Ouma <oumalyons26@gmail.com>
2023-04-27 00:01:39 +0300

    Methods as props

--- src/App.js
+++ src/App.js
@@ -2,28 +2,36 @@
 import './App.css';
 // import Message from './components/Message';
 // import Lyons from './components/Class';
-import Subscribe from './components/state';
+// import Subscribe from './components/state';
 // import Meso from './components/Test';
 // import Properties from './components/Sample';
 // import Property from './components/Sample';
 // import Destru from './components/DestrucuteClass';
-
+import ParentComponent from './components/ParentComponent';
 function App() {
   return (
     <div className="App">
-      {/* <Destru name= 'Lyons'/> */}
+    <ParentComponent/>
+
+    {/* <Destru name= 'Lyons'/> */}
+
     {/* <Property name = 'Daniel'/> */}
 
-      {/* <Properties name = 'Lyons'/>
+    {/* <Properties name = 'Lyons'/>
       <Properties name = 'John'/> */}
-      {/* <Meso/> */}
-       <Subscribe/>
-         {/* <h1>Hi Guys</h1>
-         <Message name = "Kevin"></Message>
-         <Message name = "Mercy"></Message>
-         <Message name = "Lyons"></Message>
-        <Lyons name="John" />
-        <button>Click here</button> */}
+
+    {/* <Meso/> */}
+
+    {/* <Subscribe/> */}
+
+     {/* <h1>Hi Guys</h1>
+
+     <Message name = "Kevin"></Message>
+      <Message name = "Mercy"></Message>
+      <Message name = "Lyons"></Message>
+
+      <Lyons name="John" />
+      <button>Click here</button> */}
     </div>
     
   );
---
+++ src/components/ParentComponent.js
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import ChildComponent from './ChildComponent';
+class ParentComponent extends Component{
+
+    constructor(props){
+        super(props)
+
+            this.state = {
+             ParentName : 'Tom'
+            }
+       this.greetParent = this.greetParent.bind(this); 
+    }
+    greetParent(){
+        alert(`Hello ${this.state.ParentName}`)
+    }
+render(){
+    return(
+        <div>
+            <ChildComponent greetHandler = {this.greetParent}/>
+        </div>
+    )
+}
+}
+export default ParentComponent; 
