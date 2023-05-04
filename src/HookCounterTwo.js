419392551ea3df468eedabdab1101ce57c5814f4
Lyons Ouma <oumalyons26@gmail.com>
2023-05-04 22:00:51 +0300

    taking previous state as the urgument and adding count by five

--- src/HookCounterTwo.js
+++ src/HookCounterTwo.js
@@ -3,14 +3,20 @@ import React, {useState} from "react";
 function HookCounterTwo (){
     const initialValue = 0
     const [count , finalValue] = useState(initialValue);
+    const IncrementFive = () => {
+        for(let i =0 ;i <5 ;i++){
+            finalValue (prevCount => prevCount +1)
+        }
+    }
 return(
     <div>
 
         count : {count}
         <br/>
-        <button onClick={()=>finalValue(count + 1)}>Increment</button>
+        <button onClick={()=>finalValue (prevCount => prevCount +1)}>Increment</button>
         <button onClick={()=>finalValue(initialValue)}>Reset</button>
-        <button onClick={()=>finalValue(count - 1)}>Decrement</button>
+        <button onClick={()=>finalValue (prevCount => prevCount - 1)}>Decrement</button>
+        <button onClick={()=>finalValue (prevCount => prevCount + 5)}>Increment5</button>
     </div>
 )
 }
