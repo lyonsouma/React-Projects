120e7b44a625202b6720d671acaaeac9aeea7693
Lyons Ouma <oumalyons26@gmail.com>
2023-05-04 23:39:18 +0300

    onchange function

--- src/useEffectTwo.js
+++ src/useEffectTwo.js
@@ -1,15 +1,16 @@
 import React, {useState, useEffect} from 'react'
 function UseEffectTwo(){
     const [count, outcome] = useState(0)
+    const [name, setName]  =useState('')
 
     useEffect(()=>{
         document.title = `Dialed ${count} times`
     })
 return(
     <div>
-        <input type='' ></input>
+        <input type='' value={name} onChange={(event =>setName(event.target.value))} />
         <button onClick={()=>outcome(count + 1)}>Dialed {count} times</button>
     </div>
 )
 }
-export default UseEffectTwo
+export default UseEffectTwo 
