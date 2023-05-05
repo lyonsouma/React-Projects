5372fe4b808d3eb94ec5b37f41e2eb6a57e00b68
Lyons Ouma <oumalyons26@gmail.com>
2023-05-05 12:47:23 +0300

    object map method

--- src/Map.js
+++ src/Map.js
@@ -24,7 +24,7 @@ return <div>
 */
 
                        // OBJECT
-
+/*
     let students = [
         {id :1, Name :"Lyons", course: "Software"},
         {id :2, Name :"John", course: "Cyber Sec"},
@@ -35,5 +35,27 @@ return <div>
         <h1 key = {student.id}>{student.Name},{ student.course}</h1>
     ))
 return <div>{StudentList}</div>
+*/
+
+                //OBJECT 2
+
+    let students = [
+ 
+        {id:1, Name : 'Edwin', Age: '12'},
+        {id:2, Name : 'Kevin', Age: '22'},
+        {id:3, Name : 'Lucy', Age: '90'},
+        {id:4, Name : 'Brian', Age: '122'},
+        {id:5, Name : 'Eddna', Age: '37'},
+        {id:6, Name : 'Faith', Age: '34'},
+        {id:7, Name : 'joe', Age: '15'},
+
+    ]
+
+let StudentList = students.map((student)=>(
+    <h1 key ={student.id}>{student.Name} is {student.Age} years</h1>
+))
+return <div>
+    {StudentList}
+</div>
 }
 export default Map
