2c77296e33e66bd354917635663a52ce5133101d
Lyons Ouma <oumalyons26@gmail.com>
2023-05-08 23:11:39 +0300

    objects and keys

--- src/MapThree.js
+++ src/MapThree.js
@@ -1,13 +1,13 @@
 const MapThree = () => {
 let students = [
- 
+
     {id:1, Name : 'Edwin', Age: '12'},
     {id:2, Name : 'Kevin', Age: '22'},
-    {id:3, Name : 'Lucy', Age: '90'},
+    {id:3, Name : 'Lucy',  Age: '90'},
     {id:4, Name : 'Brian', Age: '122'},
     {id:5, Name : 'Eddna', Age: '37'},
     {id:6, Name : 'Faith', Age: '34'},
-    {id:7, Name : 'joe', Age: '15'},
+    {id:7, Name : 'joe',   Age: '15'}
 
 ]
 
