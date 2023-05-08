56e657c65f205f103e201f68db316c04a704aa3f
Lyons Ouma <oumalyons26@gmail.com>
2023-05-08 23:17:17 +0300

    objects

--- src/components/input.js
+++ src/components/input.js
@@ -1,3 +1,26 @@
+/*
+const MapThree = () => {
+    let students = [
+    
+        {id:1, Name : 'Edwin', Age: '12'},
+        {id:2, Name : 'Kevin', Age: '22'},
+        {id:3, Name : 'Lucy',  Age: '90'},
+        {id:4, Name : 'Brian', Age: '122'},
+        {id:5, Name : 'Eddna', Age: '37'},
+        {id:6, Name : 'Faith', Age: '34'},
+        {id:7, Name : 'joe',   Age: '15'}
+    
+    ]
+    
+    let StudentList = students.map((student)=>(
+    <h1 key ={student.id}>{student.Name} is {student.Age} years</h1>
+    ))
+    return <div>
+    {StudentList}
+    </div>
+    }
+    */
+   
 import React, {useState} from 'react';
 import './inputStyle.css'
 function Name(){
